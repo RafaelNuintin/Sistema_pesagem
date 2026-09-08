01bb845630ac293070adb21a98506bc22a6d580b
Rafael Nuintin <rafael.nuintin@alunos.ifsuldeminas.edu.br>
2026-09-08 12:36:47 -0300

    Commit 08-09-2026 v2

--- dispositivo/processador.py
+++ dispositivo/processador.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 from collections import Counter
-
 
 class ProcessadorPesagem:
 
-    def __init__(self, confianca_minima=0.70):
+    def __init__(self, confianca_minima=0.50):
         self.confianca_minima = confianca_minima
 
     def processar(self, resultado):
@@ -28,35 +26,4 @@ class ProcessadorPesagem:
             "produto": produto,
             "contagem": dict(contador),
             "quantidade_deteccoes": len(deteccoes_validas)
-=======
-from collections import Counter
-
-
-class ProcessadorPesagem:
-
-    def __init__(self, confianca_minima=0.70):
-        self.confianca_minima = confianca_minima
-
-    def processar(self, resultado):
-
-        deteccoes_validas = [
-            d for d in resultado["deteccoes"]
-            if d["confianca"] >= self.confianca_minima
-        ]
-
-        if not deteccoes_validas:
-            return None
-
-        contador = Counter(
-            d["classe"]
-            for d in deteccoes_validas
-        )
-
-        produto = contador.most_common(1)[0][0]
-
-        return {
-            "produto": produto,
-            "contagem": dict(contador),
-            "quantidade_deteccoes": len(deteccoes_validas)
->>>>>>> c531a832e391fa3ec5abc572757318a8f855d7cf
         }
